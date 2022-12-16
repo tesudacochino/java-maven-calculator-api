7e264397d92ea87e906250f88505befebe087132
mole <smolero@gmail.com>
2022-12-16 17:14:16 +0100

    test

--- src/main/java/com/geekshubs/calculator/Calculator.java
+++ src/main/java/com/geekshubs/calculator/Calculator.java
@@ -2,4 +2,26 @@ package com.geekshubs.calculator;
 
 public class Calculator {
 
+    int _x;
+    int _y;
+    int _result;
+
+    public Calculator(int x, int y, int result) {
+        _x = x;
+        _y = y;
+        _result = result;
+    }
+
+    public int getX() {
+        return _x;
+    }
+
+    public int getY() {
+        return _y;
+    }
+
+    public int getResult() {
+        return _result;
+    }
+
 }
--- src/test/java/com/geekshubs/calculator/CalculatorTest.java
+++ src/test/java/com/geekshubs/calculator/CalculatorTest.java
@@ -1,5 +1,28 @@
 package com.geekshubs.calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
+    @Test
+    public void testSum() {
+        assertEquals(34, new Calculator(8, 26, 8 + 26).getResult());
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals(10, new Calculator(14, 4, 14 - 4).getResult());
+    }
+
+    @Test
+    public void testMult() {
+        assertEquals(12, new Calculator(3, 4, 3 * 4).getResult());
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(6, new Calculator(12, 2, 12 / 2).getResult());
+    }
 }
